b69739f4e98581dd1c67ad03b80771f7429ea25c
everton_goncalves <everton.goncalves.89@gmail.com>
2024-09-11 21:37:20 -0300

    feat: variables and constants

--- main.go
+++ main.go
@@ -4,5 +4,10 @@ import "fmt"
 
 // application entrypoint
 func main() {
-	fmt.Println("Hello world!")
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Println("Welcome to", conferenceName)
+	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
 }
