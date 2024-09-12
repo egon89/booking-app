b45d362d81e0d7d430ed717a5f6793e29144b706
everton_goncalves <everton.goncalves.89@gmail.com>
2024-09-11 22:13:49 -0300

    feat: data types
    - int
    - short way for variable declaration
    - variable without initial value

--- main.go
+++ main.go
@@ -4,10 +4,21 @@ import "fmt"
 
 // application entrypoint
 func main() {
-	var conferenceName = "Go Conference"
+	// var conferenceName = "Go Conference"
+	conferenceName := "Go Conference" // short way
 	const conferenceTickets = 50
-	var remainingTickets = 50
+	var remainingTickets uint = 50
+
+	fmt.Printf("conferenceName is %T, conferenceTicktes is %T, remainingTickets is %T\n", conferenceName, conferenceTickets, remainingTickets)
+	// conferenceName is string, conferenceTicktes is int, remainingTickets is uint
 
 	fmt.Printf("Welcome to %v\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+
+	var userName string
+	var userTickets int
+
+	userName = "John"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
 }
--- readme.md
+++ readme.md
@@ -10,3 +10,24 @@ go mod init booking-app
 ```bash
 go run main.go
 ```
+
+## Constants and variables
+- We can use the short way to declare variables
+    ```go
+    conferenceName := "Go Conference"
+    ```
+- We cannot use it
+    - for constants
+    - for explicity data type like `variableName uint := 2`
+
+## Data Types
+
+### Int
+| Go | Java |
+| -- | -- |
+| int8  | byte  |
+| int16 | short |
+| int32 | integer |
+| int64 | long |
+
+Use `uint8`, `uint16`, `uint32` and `uint64` for positive numbers
